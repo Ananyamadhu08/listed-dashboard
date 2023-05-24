a039936215fc057b5a3c6c31587a370ab45b7209
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-05-24 19:37:30 +0530

    feat: created home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
-import { DashboardCard, Header, Sidebar } from '../components';
+import {
+  DashboardCard,
+  Header,
+  ScheduleCardsContainer,
+  Sidebar,
+} from '../components';
 import {
   TotalLikesIcon,
   TotalRevenueIcon,
@@ -11,10 +16,10 @@ function Home() {
     <div className="grid grid-cols-9 grid-rows-6 h-screen">
       <Sidebar />
 
-      <div className="row-span-5 xl:row-span-6 col-span-9 xl:col-span-7 mx-5">
+      <div className="row-span-5 xl:row-span-6 col-span-9 xl:col-span-7 mx-10 xl:ml-1 xl:mr-5">
         <Header />
 
-        <div className="grid grid-cols-4 mt-8">
+        <div className="grid grid-cols-4 gap-x-6 mt-8">
           <DashboardCard
             bgColor="bg-[#DDEFE0]"
             imgSrc={TotalRevenueIcon}
@@ -42,6 +47,20 @@ function Home() {
             title="Total Users"
             value="892"
           />
+        </div>
+
+        <div className="h-3/5">
+          <div className="bg-white mt-5 mb-7 rounded-[1.25rem] h-2/4">
+            Line Chart
+          </div>
+
+          <div className="grid grid-cols-2 gap-x-9 gap-y-6 mb-9 md:mb-1 h-full md:h-[45%]">
+            <div className="bg-white col-span-2 sm:col-span-1 rounded-[1.25rem] h-">
+              Pie Chart
+            </div>
+
+            <ScheduleCardsContainer />
+          </div>
         </div>
       </div>
     </div>
