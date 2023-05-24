4f2f06ee5e0bac608067bbe75a5473c0e672d336
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-05-24 07:20:02 +0530

    feat: created dashboard card component

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,10 @@ import SettingIcon from './setting_icon.svg';
 import BellIcon from './bell_icon.svg';
 import SearchIcon from './search_icon.svg';
 import UserImage from './user_image.svg';
+import TotalRevenueIcon from './total_revenue_icon.svg';
+import TotalTransactionsIcon from './total_transactions_icon.svg';
+import TotalLikesIcon from './total_likes_icon.svg';
+import TotalUsersIcon from './total_users_icon.svg';
 
 export {
   DashboardIcon,
@@ -16,4 +20,8 @@ export {
   BellIcon,
   SearchIcon,
   UserImage,
+  TotalRevenueIcon,
+  TotalTransactionsIcon,
+  TotalLikesIcon,
+  TotalUsersIcon,
 };
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import LoginForm from './LoginForm';
 import RegisterForm from './RegisterForm';
 import Sidebar from './Sidebar';
 import Header from './Header';
+import DashboardCard from './DashboardCard';
 
-export { LoginForm, RegisterForm, Sidebar, Header };
+export { LoginForm, RegisterForm, Sidebar, Header, DashboardCard };
