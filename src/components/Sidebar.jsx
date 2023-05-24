ff307a57a30769d1767d2cac8d5295599cc33b8a
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2023-05-24 19:35:56 +0530

    feat: created sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,33 +8,33 @@ import {
 
 function Sidebar() {
   return (
-    <div className="bg-black row-span-1 xl:row-span-6 col-span-9 h-2/4 xl:col-span-2 w-[95%] xl:h-[90%] m-auto xl:my-10 text-white rounded-[1.875rem] p-8 xl:py-[3.75rem] xl:px-[3.125rem] justify-center flex sm:justify-between items-center xl:block relative">
+    <div className="bg-black row-span-1 xl:row-span-6 col-span-9 h-2/4 xl:col-span-2 w-[97%] xl:w-[84%] xl:h-[94%] m-auto xl:mx-6 text-white rounded-[1.875rem] p-8 xl:py-[3.75rem] xl:px-[3.125rem] justify-center flex sm:justify-between items-center xl:block relative">
       <h2 className="hidden sm:flex sm:mr-20 justify-center items-center xl:block font-bold text-3xl xl:text-4xl xl:mb-[3.75rem]">
         Board.
       </h2>
 
       <div className="flex xl:flex-col gap-8 md:gap-16 xl:gap-10">
-        <div className="flex gap-6 xl:text-lg">
+        <div className="flex gap-5 xl:text-lg">
           <img src={DashboardIcon} alt="dashboard icon" />
           <a className="hidden xl:block">Dashboard</a>
         </div>
 
-        <div className="flex gap-6 text-lg">
+        <div className="flex gap-5 text-lg">
           <img src={TransactionIcon} alt="dashboard icon" />
           <a className="hidden xl:block">Transactions</a>
         </div>
 
-        <div className="flex gap-6 text-lg">
+        <div className="flex gap-5 text-lg">
           <img src={ScheduleIcon} alt="dashboard icon" />
           <a className="hidden xl:block">Schedules</a>
         </div>
 
-        <div className="flex gap-6 text-lg">
+        <div className="flex gap-5 text-lg">
           <img src={UserIcon} alt="dashboard icon" />
           <a className="hidden xl:block">Users</a>
         </div>
 
-        <div className="flex gap-6 text-lg">
+        <div className="flex gap-5 text-lg">
           <img src={SettingIcon} alt="dashboard icon" />
           <a className="hidden xl:block">Settings</a>
         </div>
